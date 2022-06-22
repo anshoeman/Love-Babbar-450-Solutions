5df55a2e11557e4af37b16b23fce06f1e12727f3
anshoeman <anshumannehru2002@gmail.com>
2022-06-22 21:09:57 +0530

    binary_tree

--- Binary_Tree/Right_View.cpp
+++ Binary_Tree/Right_View.cpp
@@ -2,11 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Tree Node
 struct Node
 {
     int data;
-    struct Node *left;
-    struct Node *right;
+    Node* left;
+    Node* right;
 };
 // Utility function to create a new Tree Node
 Node* newNode(int val)
@@ -15,120 +16,143 @@ Node* newNode(int val)
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
+
+
+ // } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Should return  right view of tree
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+       vector<int> v;
+       if(!root)
+            return v;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){/*take levels and print last level*/
+                Node* curr = q.front();
+                q.pop();
+                if(size==0)
+                v.push_back(curr->data);
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+        }
+    return v;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
 // Function to Build Tree
 Node* buildTree(string str)
-{   
+{
     // Corner Case
     if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
     for(string str; iss >> str; )
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
     while(!queue.empty() && i < ip.size()) {
-            
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
         if(currVal != "N") {
-                
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
         if(i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
         if(currVal != "N") {
-                
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
-vector<int> reverseLevelOrder(Node* root);
 
-int main()
-{
-
+int main() {
     int t;
-    scanf("%d ",&t);
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
     while(t--)
     {
         string s;
         getline(cin,s);
         Node* root = buildTree(s);
-        vector<int> result = reverseLevelOrder(root);
-        for (int i = 0; i < result.size(); ++i)
-        {
-            cout<<result[i]<<" ";
+
+        Solution ob;
+        vector<int> vec = ob.rightView(root);
+        for(int x : vec){
+            cout<<x<<" ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    return 1;
-}// } Driver Code Ends
-
-
-/*   
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-}; */
-vector<int> reverseLevelOrder(Node *root)
-{
-    queue<Node*> q;
-    vector<int> ans;
-    if(!root)
-    return ans;
-    q.push(root);
-    while(!q.empty()){
-        Node* t = q.front();/*create a new node*/
-        ans.push_back(t->data);
-        if(t->right)
-            q.push(t->right);
-        if(t->left)
-            q.push(t->left);
-        q.pop();
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
-
+    return 0;
 }
+
+  // } Driver Code Ends
